831c78c36553f4791f7e7007ed88fea0d6ee0dde
Ranjith Kumar S <sranjithkumar.1205@gmail.com>
2026-03-22 23:50:29 +0530

    Initial commit: Payroll Job Monitor Backend project setup

---
+++ src/main/java/com/example/payroll_job_monitor_backend/PayrollJobMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.payroll_job_monitor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollJobMonitorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollJobMonitorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/payroll_job_monitor_backend/PayrollJobMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payroll_job_monitor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollJobMonitorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
